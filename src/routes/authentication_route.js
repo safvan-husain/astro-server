19c6eb90e818b94a95a80974358b3040331f90c3
safvan-husain <safvanhusain009@gmail.com>
2023-12-08 16:28:19 +0530

    edit user with new number

--- src/routes/authentication_route.js
+++ src/routes/authentication_route.js
@@ -118,7 +118,7 @@ router.post("/register", async (req, res) => {
 
 router.post("/edit-user", async (req, res) => {
   try {
-    var user = await User.findOne({ phone: req.body.phone });
+    var user = await User.findOne({ phone: req.query.phone });  
     if (user != null) {
       user = await user.updateProfile(req.body);
       res.status(200).json(user);
--- test.js
+++ test.js
@@ -1,36 +1,4 @@
-import fast2sms from "fast-two-sms";
-import dotenv from "dotenv";
-import unirest from "unirest";
+import { Password } from "./src/utils/password_hash.js";
 
-dotenv.config();
 
-const key = process.env.FAST2SMS_KEY_SECRET;
-
-  var req = unirest("GET", "https://www.fast2sms.com/dev/bulkV2");
-
-  req.query({
-    "authorization": `${key}`,
-    "variables_values": "5599",
-    "route": "otp",
-    "numbers": "7907320942"
-  });
-  
-  req.headers({
-    "cache-control": "no-cache"
-  });
-  
-  
-  req.end(function (res) {
-    if (res.error) throw new Error(res.error);
-  
-    console.log(res.body);
-  }); 
-
- 
-
-// fast2sms.sendMessage(options).then(response=> {
-//     console.log(response);
-//     console.log('after response');
-// }).catch(error=> {
-//     console.log(error); 
-// }) 
+Password.hashPassword("123").then(s=> console.log(s));
